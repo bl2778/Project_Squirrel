8da0b2292d226fdc8d21f4047158516496dfb2a7
Zifan Nian <mac@zifan-niandembp.home>
2019-12-08 18:34:51 -0500

    Add 3 views
    Commit these views

--- sightings/views.py
+++ sightings/views.py
@@ -1,3 +1,45 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
 
+from .models import Squirrel
+from .forms import SquirrelForm
+
+def map(request):
+    squirrels = Squirrel.objects.all()
+    return render(request, 'sightings/map.html', {'squirrels':squirrels})
+
+
+def edit_squirrel(request, Unique_squirrel_id):
+    squirrel = Squirrel.objects.get(Unique_squirrel_id=Unique_squirrel_id)
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST, instance=squirrel)
+        # check data with form
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/list')
+    else:
+        form = SquirrelForm(instance=squirrel)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'sightings/edit.html', context)
+
+def add_squirrel(request):
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sightings:list')
+
+    else:
+        form = SquirrelForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'sightings/add.html', context)
 # Create your views here.
